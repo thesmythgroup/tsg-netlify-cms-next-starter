5325ab414d998b8d2b494b72b18f052fe92d50de
Chris Leyva <cleyva002@gmail.com>
2022-07-20 18:23:06 -0400

    refactor: replace class components with functions
    This may be controversial, but FunctionComponents seem to be
    the "future" in React-world. Let's try to keep up.

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './header/Header';
 import Footer from './footer/Footer';
 
-export default class Layout extends Component {
-  render(): JSX.Element {
-    const { children } = this.props;
-
-    return (
-      <div className='container mx-auto px-2'>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </div>
-    );
-  }
+interface LayoutProps {
+  children: React.ReactNode;
 }
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className='container mx-auto px-2'>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
